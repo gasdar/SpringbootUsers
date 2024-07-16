e45135e7e52bbeafc917e8ccff270ff75e762ed7
gasdar <gottafindshape@gmail.com>
2024-07-16 16:58:04 -0400

    Reconstrucción proyecto sprinboot users

---
+++ src/test/java/com/rolan/springboot/users/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolan.springboot.users.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
